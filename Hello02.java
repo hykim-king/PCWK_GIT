4fa630b01c0de11647d74dd84651edb9c7c463c6
hykim <dnjsxn26@gmail.com>
2022-01-05 14:18:14 +0900

    충돌테스트

--- Hello02.java
+++ Hello02.java
@@ -1,5 +1,6 @@
 public class Hello02{
      public static void main(String [] args){
            System.out.println("Hello, world");
+           System.out.println("Hello, world"); //충돌테스트
      }
 }
